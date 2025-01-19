6ecee9df2e8c5e0ddcf7915e71b9045c782ac7de
Alban <alban.kora@yum.com>
2025-01-19 22:04:22 +0000

    WIP

--- public/merchant/js/script.js
+++ public/merchant/js/script.js
@@ -7,3 +7,29 @@ new Chartist.Line('#traffic-chart', {
   low: 0,
   showArea: true
 });
+
+document.addEventListener("DOMContentLoaded", function(){
+  document.querySelectorAll('.sidebar .nav-link').forEach(function(element){
+    element.addEventListener('click', function (e) {
+
+      let nextEl = element.nextElementSibling;
+      let parentEl  = element.parentElement;	
+
+        if(nextEl) {
+            e.preventDefault();	
+            let mycollapse = new bootstrap.Collapse(nextEl);
+            if(nextEl.classList.contains('show')){
+              mycollapse.hide();
+            } else {
+                mycollapse.show();
+                // find other submenus with class=show
+                var opened_submenu = parentEl.parentElement.querySelector('.submenu.show');
+                // if it exists, then close all of them
+                if(opened_submenu){
+                  new bootstrap.Collapse(opened_submenu);
+                }
+            }
+        }
+    }); // addEventListener
+  }) // forEach
+});
